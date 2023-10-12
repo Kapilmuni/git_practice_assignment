8aa01fca8ca4bd87cde90efdc3919ff6e2e30fd5
Kapilmuni <kapillilhare2@gmail.com>
2023-10-12 12:27:50 +0530

    check the number if it is prime or not

---
+++ prime.js
@@ -0,0 +1,16 @@
+let A=11;
+let count=0;
+
+  for(let i=2;i<=A;i++){
+    if(A%i==0){
+      count+=1;
+    }
+  }
+  if (count){
+     console.log("False" )
+    
+  }
+  else{
+    console.log("True ")
+  }  
+
